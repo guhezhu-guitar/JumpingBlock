9a59a5df86f0ed6665a0a31b674d23e671838d7e
guhezhu-guitar <3095677916@qq.com>
2025-09-27 14:24:03 +0800

    新增跳跃和地图生成

--- assets/Scripts/PlayerController.ts
+++ assets/Scripts/PlayerController.ts
@@ -1,13 +1,20 @@
-import { _decorator, Component, EventMouse, Input, input, Node } from 'cc';
+import { _decorator,  Animation,  Component, EventMouse, Input, input, Node, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('PlayerController')
 export class PlayerController extends Component {
 
     private _startTime =false;
+    private _startJump =false;
     private _jumpTime =0.2;
     private _curJumpTime = 0;
     private _jumpSpeed = 0;
+    private _targetPos = new Vec3();
+    private _curPos = new Vec3();
+
+    //引入body上面的Animation组件
+    @property(Animation)
+    public bodyAnim : Animation =null;
 
     start() {
         input.on(Input.EventType.MOUSE_DOWN,this.onMouseDown,this)
@@ -27,16 +34,50 @@ export class PlayerController extends Component {
         // const curPos = this.node.position;
         // this.node.setPosition(curPos.x+40*step,curPos.y,curPos.z);
 
+        //在移动的时候再点击就不触发后面的代码
+        if(this._startTime){
+            return;
+        }
+
+        //获取动画的名称,方法是使用三元运算符
+        const animName = step == 1? 'jumpOneStep':'jumpTwoStep';
+
+
+        //获取动画的状态
+        const animState =this.bodyAnim.getState(animName);
+        //获取动画的时间
+        this._jumpTime=animState.duration;
+
+        const moveLength = step*40;
         this._startTime =true;  //跳跃开始  
+        this._startJump =true;
         this._curJumpTime = 0;  //开始计时
         this._jumpSpeed =step*40/this._jumpTime;
+        //得到当前的位置
+        this.node.getPosition(this._curPos);
+        this._targetPos =new Vec3(this._curPos.x+moveLength,this._curPos.y,this._curPos.z);
+        Vec3.add(this._targetPos,this._curPos,new Vec3(moveLength,0,0));
+
+        //跳跃的时候播放动画
+        // if(step==1){
+        // this.bodyAnim.play('jumpOneStep');
+        // }else if(step==2){
+        // this.bodyAnim.play('jumpTwoStep');
+        // }
+
+        //让动画的时间和跳跃时间重合,使用统一的jumpTime的值
+        this.bodyAnim.play(animName)
+        
     }
+
 
     protected update(dt: number): void {
         if(this._startTime){
             this._curJumpTime +=dt;
             if(this._curJumpTime > this._jumpTime){
                 this._startTime =false;
+                //移动的精准性
+                this.node.setPosition(this._targetPos)
             }else{
                 const curPos =this.node.position;
                 this.node.setPosition(curPos.x+this._jumpSpeed*dt,curPos.y,curPos.z);
